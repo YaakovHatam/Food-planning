810399c578013cdb9472873170de119f220edf1b
RivkaShelli <93613379+RivkaShelli@users.noreply.github.com>
2022-03-23 20:16:07 +0200

    Object planning of fast food

---
+++ Fast.Food.Interface.ts
@@ -0,0 +1,11 @@
+
+export interface FastFood {
+    id: number;
+    name: string;
+    carbohydrates: number;
+    proteins: number;
+    fats: number;
+    totalCalories: number;
+    kosher: string;
+    foodGroup: string; // dairy/meaty/parve
+}
