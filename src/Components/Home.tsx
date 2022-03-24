b1a7e32ab2a442ebda1904e12afeca4da0e99f69
Amicht <vicolicht@gmail.com>
2022-03-24 03:18:14 +0200

    Search form

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,7 +1,13 @@
+import { FastFood } from "../Interfaces/Fast.Food.Interface"
+import Search from "./Search"
 
-const Home = () => {
+const Home = (props:{dbArr:FastFood[],setMenusArr:any}) => {
   return (
-    <div>Home</div>
+    <div className="container" style={{direction:"rtl"}}>
+      <h2 className="text-center">חיפוש תפריט בהתאמה אישית</h2>
+      <h6 className="text-danger text-center">(לפי 100 גרם)</h6>
+      <Search dbArr={props.dbArr} setMenusArr={props.setMenusArr}/>
+    </div>
   )
 }
 
---
+++ src/Components/Search.tsx
@@ -0,0 +1,57 @@
+import { createRef } from 'react';
+import {Form, Button} from 'react-bootstrap';
+import { FastFood } from '../Interfaces/Fast.Food.Interface';
+
+const Search = (props:{dbArr:FastFood[],setMenusArr:any}) =>{
+    const carboMin = createRef<HTMLInputElement>();
+    const carboMax = createRef<HTMLInputElement>();
+    const protMin = createRef<HTMLInputElement>();
+    const protMax = createRef<HTMLInputElement>();
+    const fatMin = createRef<HTMLInputElement>();
+    const fatMax = createRef<HTMLInputElement>();
+    const setSearchResults = () =>{ 
+        if(fatMin.current?.value && fatMax.current?.value &&
+        protMin.current?.value && protMax.current?.value &&
+        carboMax.current?.value && carboMin.current?.value !== ''){
+            const menuResults:FastFood[] = props.dbArr.filter(m => (m.fats<+fatMax.current?.value!) && 
+            (m.fats>+fatMin.current?.value!) &&
+            (m.carbohydrates<+carboMax.current?.value!) && 
+            (m.carbohydrates>+carboMin.current?.value!) &&
+            (m.proteins<+protMax.current?.value!) && 
+            (m.proteins>+protMin.current?.value!))
+            props.setMenusArr(menuResults); //state function
+        }
+    }
+  return (
+        <Form  className="col-8 mx-auto ">
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>פחמימות:</Form.Label>
+                <div className='d-flex'>
+                <Form.Control ref={carboMin} required className='col' type="Number" placeholder="מינמום" />
+                <Form.Control ref={carboMax} required className='col' type="Number" placeholder="מקסימום" />
+                </div>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>שומנים:</Form.Label>
+                <div className='d-flex'>
+                <Form.Control ref={fatMin} required className='col' type="Number" placeholder="מינמום" />
+                <Form.Control ref={fatMax} required className='col' type="Number" placeholder="מקסימום" />
+                </div>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>חלבונים:</Form.Label>
+                <div className='d-flex'>
+                <Form.Control ref={protMin} required className='col' type="Number" placeholder="מינמום" />
+                <Form.Control ref={protMax} required className='col' type="Number" placeholder="מקסימום" />
+                </div>
+            </Form.Group>
+            <div className='text-start'>
+                <Button variant="dark" onClick={setSearchResults}>
+                    חפש תפריט
+                </Button>
+            </div>
+        </Form>
+  )
+}
+
+export default Search
