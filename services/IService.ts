0ad679a0561e4cc17014e6fecf948f1a2fe1582b
OSNAT DAYAN <93711505+qpasdmx@users.noreply.github.com>
2022-03-25 09:10:33 +0300

    Delete IService.ts

--- services/IService.ts
+++
@@ -1,8 +0,0 @@
-import { FastFood } from "../Interfaces/Fast.Food.Interface";
-
-export interface IService {
-    AddItem(food: FastFood): void;
-    getAll(): FastFood[];
-    removeItem(id: number): void;
-
-}
