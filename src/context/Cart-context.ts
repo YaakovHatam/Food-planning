cce7561e51731e3865ebd1a4959c2631035af641
OSNAT DAYAN <93711505+qpasdmx@users.noreply.github.com>
2022-03-28 21:59:15 +0300

    Update Cart-context.ts
    change the type

--- src/context/Cart-context.ts
+++ src/context/Cart-context.ts
@@ -1,37 +1,46 @@
-import { createContext } from "react";
-import { FastFood, Food } from "../Interfaces/Fast.Food.Interface";
+import { createContext } from "react"
+import {  Food } from "../Interfaces/Fast.Food.Interface";
 
-var cart: Food[] = JSON.parse(localStorage.getItem("cart")!) || [];
+type MyCartValueTypes = {
+    cart: Food[];
+    GetAllItems(): Food[];
+    removeItem(id: number): void;
+    AddItem(f: Food): void;
+    itemsList(): string[];
+    summary(): Partial<Food>;
+};
 
-export const MyCartValue = {
-  GetAllItems: () => cart,
-  AddItem: (f: Food) => {
-    cart.push(f);
-    localStorage.setItem("cart", JSON.stringify(cart));
-    console.log(cart);
-  },
-  removeItem: (id: number) => {
-    if (id !== undefined) {
-      let idArr = cart.map((f) => f.id);
-      let index = idArr.indexOf(id);
-      if (index !== -1) {
-        cart.splice(index, 1);
-        localStorage.setItem("cart", JSON.stringify(cart));
-      }
-    }
-  },
-  itemsList() {
-    return cart.map((item: Food) => item.name);
-  },
-  summary() {
-    return cart.reduce((initVal: Partial<Food>, item: Food) => {
-      initVal.carbohydrates! += item.carbohydrates;
-      initVal.proteins! += item.proteins;
-      initVal.fats! += item.fats;
-      initVal.totalCalories! += item.totalCalories;
-      return initVal;
-    }, {});
-  },
+export const MyCartValue: MyCartValueTypes = {
+    cart: JSON.parse(localStorage.getItem("cart")!) || [],
+    GetAllItems: () => MyCartValue.cart,
+    AddItem: (f: Food) => {
+        MyCartValue.cart.push(f);
+        localStorage.setItem("cart", JSON.stringify(MyCartValue.cart));
+        console.log(MyCartValue.cart);
+    },
+    removeItem: (id: number) => {
+        if (id !== undefined) {
+            let idArr = MyCartValue.cart.map((f: Food) => f.id);
+            let index = idArr.indexOf(id);
+            if (index !== -1) {
+                MyCartValue.cart.splice(index, 1);
+                console.log(MyCartValue.cart);
+                localStorage.setItem("cart", JSON.stringify(MyCartValue.cart));
+            }
+        }
+    },
+    itemsList() {
+        return MyCartValue.cart.map((item: Food) => item.name);
+    },
+    summary() {
+        return MyCartValue.cart.reduce((initVal: Partial<Food>, item: Food) => {
+            initVal.carbohydrates! += item.carbohydrates;
+            initVal.proteins! += item.proteins;
+            initVal.fats! += item.fats;
+            initVal.totalCalories! += item.totalCalories;
+            return initVal;
+        }, {});
+    },
 };
 export const MyCartContext = createContext(MyCartValue);
 export const MyCartProvider = MyCartContext.Provider;
